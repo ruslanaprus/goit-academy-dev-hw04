15a565fee3e68bc15264cd0d976c8d7abea8ef67
ruslana <ruslana.prus@ukr.net>
2024-08-22 14:12:31 +0200

    removed CTE

--- find_youngest_eldest_workers.sql
+++ find_youngest_eldest_workers.sql
@@ -1,29 +1,27 @@
 -- Task 6
 
-WITH oldest AS (
-	SELECT
-		min(birthday) AS earliest_dob
-	FROM
-		worker
-),
-youngest AS (
-	SELECT
-		max(birthday) AS latest_dob
-	FROM
-		worker
-)
 SELECT
 	'OLDEST' AS TYPE,
 	w.name,
 	w.birthday
 FROM
-	Worker w
-	INNER JOIN oldest o ON w.birthday = o.earliest_dob
-UNION ALL
-SELECT
-	'YOUNGEST' AS TYPE,
-	w.name,
-	w.birthday
-FROM
-	Worker w
-	INNER JOIN youngest y ON w.birthday = y.latest_dob;
+	worker w
+WHERE
+	w.birthday = (
+		SELECT
+			MIN(birthday)
+		FROM
+			worker)
+	UNION ALL
+	SELECT
+		'YOUNGEST' AS TYPE,
+		w.name,
+		w.birthday
+	FROM
+		worker w
+	WHERE
+		w.birthday = (
+			SELECT
+				MAX(birthday)
+			FROM
+				worker);
